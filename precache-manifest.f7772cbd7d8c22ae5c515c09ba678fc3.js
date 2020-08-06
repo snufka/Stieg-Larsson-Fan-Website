084d3ec7b213896750e7e420fbf2e30033709f32
snufka <52801250+snufka@users.noreply.github.com>
2020-08-06 23:07:45 +0200

    build commit

---
+++ precache-manifest.f7772cbd7d8c22ae5c515c09ba678fc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c0d245f0910eeb58044834ac0f5f3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61e6b1de10228ecab0ad",
+    "url": "/static/css/main.39e5b59c.chunk.css"
+  },
+  {
+    "revision": "db5d96df4a0ea0bee55f",
+    "url": "/static/js/2.b896f349.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b896f349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e6b1de10228ecab0ad",
+    "url": "/static/js/main.0a2da74d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
